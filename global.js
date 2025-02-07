3f20d80159cd55fab39ef920ce02a3e6e309665c
JeffersonChen888 <jec068@ucsd.edu>
2025-02-06 17:44:07 -0800

    step 0 lab 5

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
-console.log('IT’S ALIVE!');
+console.log('IT'S ALIVE!');
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
@@ -186,7 +186,10 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
         article.innerHTML = `
             <${headingLevel}>${project.title}</${headingLevel}>
             <img src="${project.image}" alt="${project.title}">
-            <p>${project.description}</p>
+            <div class="project-content">
+                <p>${project.description}</p>
+                <p class="project-year">${project.year}</p>
+            </div>
         `;
         containerElement.appendChild(article);
     });
